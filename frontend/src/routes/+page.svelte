85dc52483d196f7b0e80e367cce4c4e8b8c2e0e6
Nand Patel <nand.pats@gmail.com>
2024-09-04 13:38:34 -0700

    add details to home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,6 +5,49 @@
 
 <section>
 	<h1>brat nite</h1>
+	<table>
+		<tr>
+			<td>
+				<h2>who?</h2>
+			</td>
+			<td>
+				<h3>you!</h3>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h2>what?</h2>
+			</td>
+			<td>
+				<h3>brat nite pre!</h3>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h2>when?</h2>
+			</td>
+			<td>
+				<h3>september 7, 7pm</h3>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h2>where?</h2>
+			</td>
+			<td>
+				<h3>my address will not be on the internet so just ask me lol</h3>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h2>why?</h2>
+			</td>
+			<td>
+				<h3>because brat</h3>
+			</td>
+		</tr>
+	</table>
+	<p>p.s. please check the other tabs for details and to volunteer to help me :)</p>
 </section>
 
 <style>
@@ -18,5 +61,29 @@
 
 	h1 {
 		width: 100%;
+		font-size: 50px;
+	}
+	h2 {
+		font-size: 30px;
+		font-weight: 800;
+	}
+
+	h3 {
+		font-size: 20px;
+		font-weight: 100;
+	}
+
+	table {
+		table-layout: fixed;
+		width: 100%;
+	}
+	td {
+		padding: 5px;
+	}
+	tr td:nth-child(1) {
+		text-align: end;
+	}
+	tr td:nth-child(2) {
+		text-align: start;
 	}
 </style>
